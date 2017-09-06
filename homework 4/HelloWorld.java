d4fbf3c6837f7049c376148e2258a19f2c62f838
Scott Brady <scott@prolio.com>
2017-09-06 09:50:02 -0400

    I think I did it

---
+++ homework 4/HelloWorld.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Scott Brady
+   Date Due:   8/26/2017
+ ******************************************************************************/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		string message;
+        
+        // Print out a message to the whole world!
+		System.out.println("Hello World!");
+	}
+}
