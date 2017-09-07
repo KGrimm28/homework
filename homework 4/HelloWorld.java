fe1940da213ba2e6ed992156bf926f71520496af
Scott Brady <scott@prolio.com>
2017-09-07 10:29:03 -0400

    Tabs

--- homework 4/HelloWorld.java
+++ homework 4/HelloWorld.java
@@ -10,9 +10,9 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		String message;
-        message = "LeBron James is GOAT!";
-        
-        // Print out a message to the whole world!
+		message = "LeBron James is GOAT!";
+		
+		// Print out a message to the whole world!
 		System.out.println(message);
 	}
 }
