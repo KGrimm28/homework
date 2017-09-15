1cf96e91e45731811938915a2bc81729860db9c6
Scott Brady <scott@prolio.com>
2017-09-15 09:52:28 -0400

    Took long enough, but finally

---
+++ homework 5/HelloWorld.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Kyle Grimm
+   Date Due:   9/11/2017
+ ******************************************************************************/
+ import java.util.Scanner;
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard;
+		keyboard = new Scanner(System.in);
+		String name;
+		System.out.println("Hello! What is your name?");
+		name = keyboard.nextLine();
+		System.out.println("Hello! " + name);
+
+
+
+		// Print out a message to the whole world!
+		
+	}
+}
