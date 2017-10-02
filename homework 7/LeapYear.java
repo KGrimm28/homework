16ba2f6e0f1a9ff3d91c443a828b26bcb339292b
KGrimm28 <VaporGrimm28@icloud.com>
2017-10-02 09:59:37 -0400

    Leap Year

---
+++ homework 7/LeapYear.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Kyle Grimm
+   Date Due:   10/2/2017
+ ******************************************************************************/
+ import java.util.Scanner;
+
+public class LeapYear
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int year;
+
+		System.out.print("Enter a year: ");
+		year = keyboard.nextInt();
+
+		if (year % 4 == 0)
+		{
+			if (year % 100 == 0)
+			{
+				if (year % 400 == 0)
+				{
+					System.out.print("That's a leap year :D");
+				}
+				else 
+				{
+					System.out.print("That's a regular year lol");
+				}
+			}
+			else 
+			{
+				if (year % 400 == 0)
+				{
+					System.out.print("That's a leap year :D");
+				}
+				else
+				{
+					System.out.print("That's a regular year lol");
+				}
+			}
+		}
+		else
+		{
+			System.out.print("That's a regular year lol");
+		}
+	}
+}
